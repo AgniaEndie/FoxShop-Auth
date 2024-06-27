d96d4ed0cb99664adf2db9ba49690e49ddccae03
agniaendie <endienasg3@gmail.com>
2024-06-27 05:11:19 +0500

    update model

--- src/main/kotlin/ru/agniaendie/authservice/model/AuthModel.kt
+++ src/main/kotlin/ru/agniaendie/authservice/model/AuthModel.kt
@@ -6,7 +6,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 
-data class AuthModel(@Id var uuid: String, @JvmField var username: String?, @JvmField var password: String, var role: Role, var email:String) : UserDetails {
+data class AuthModel(@Id var uuid: String?, @JvmField var username: String?, @JvmField var password: String, var role: Role, var email:String) : UserDetails {
     override fun getAuthorities(): List<SimpleGrantedAuthority> {
         return listOf(SimpleGrantedAuthority(role.name))
     }
--- src/main/kotlin/ru/agniaendie/authservice/service/AuthService.kt
+++ src/main/kotlin/ru/agniaendie/authservice/service/AuthService.kt
@@ -31,7 +31,7 @@ class AuthService(
 
     suspend fun createAuthModel(request: CreateAuthModelRequest): Mono<AuthModel> {
         val user =
-            AuthModel("", request.username, request.password, Role.ROLE_NORMAL, request.email)
+            AuthModel(null, request.username, request.password, Role.ROLE_NORMAL, request.email)
         return authRepository.save(user)
     }
 
