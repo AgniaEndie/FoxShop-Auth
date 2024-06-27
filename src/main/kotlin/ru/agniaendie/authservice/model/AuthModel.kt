01d8be9f128fbf9022e27dedbf6bf7cc3f7dc5d5
agniaendie <endienasg3@gmail.com>
2024-06-27 06:07:45 +0500

    implements Persistable in AuthModel.kt

--- src/main/kotlin/ru/agniaendie/authservice/model/AuthModel.kt
+++ src/main/kotlin/ru/agniaendie/authservice/model/AuthModel.kt
@@ -3,17 +3,32 @@ package ru.agniaendie.authservice.model
 import com.fasterxml.jackson.annotation.JsonIgnore
 import lombok.Data
 import org.springframework.data.annotation.Id
+import org.springframework.data.domain.Persistable
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 @Data
-data class AuthModel(@Id var uuid: String, @JvmField var username: String?, @JvmField var password: String, var role: Role, var email:String) : UserDetails {
+data class AuthModel(
+    @Id var uuid: String,
+    @JvmField var username: String?,
+    @JvmField var password: String,
+    var role: Role,
+    var email: String
+) : UserDetails, Persistable<String> {
     override fun getAuthorities(): List<SimpleGrantedAuthority> {
         return listOf(SimpleGrantedAuthority(role.name))
     }
+
     @JsonIgnore
     override fun getPassword() = password
 
     override fun getUsername() = username
+    override fun getId(): String? {
+        return this.uuid
+    }
+
+    override fun isNew(): Boolean {
+        return id == null
+    }
 
 }
